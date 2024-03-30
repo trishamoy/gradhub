0f7833edf393ed81e8d51b7ddaa937a72ded8b4c
trishabalmes <trishac.balmes@gmail.com>
2024-03-31 01:04:06 +0800

    replace with a pool

--- database.js
+++ database.js
@@ -1,19 +1,12 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'gradhub',
-    connectionLimit: 10, // Adjust the limit as per your requirements
-    connectTimeout: 10000, // Add a timeout for the connection
-
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
-    // connectionLimit: 10,
-    // connectTimeout: 10000,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectionLimit: 10,
+    connectTimeout: 10000,
 });
 
 // Handle connection errors
